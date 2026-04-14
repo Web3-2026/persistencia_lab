1bab4ab06e07deadec144f448e41857edb010c46
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2026-04-14 15:27:21 -0300

    Add ProfessorRepository class for database operations

---
+++ ProfessorRepository2.java
@@ -0,0 +1,51 @@
+public class ProfessorRepository {
+
+    private Connection conexao;
+
+    public ProfessorRepository() {
+
+        ConexaoFactory conexaoFactory = new ConexaoFactory();
+        conexao = conexaoFactory.getConnection();
+    }
+
+    public void getProfessors() {
+
+        Statement instrucao = null;
+        ResultSet resultados = null;
+
+        try {
+            instrucao = conexao.createStatement();
+            resultados = instrucao.executeQuery("SELECT * FROM professores");
+
+            while (resultados.next()) {
+                System.out.println(resultados.getString("nome"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    public void insert(Professor professor) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conexao.prepareStatement(
+                    "INSERT INTO professores (nome, email, data_nascimento, salario_base, curso_id) VALUES (?, ?, ?, ?, ?)");
+
+            statement.setString(1, professor.getNome());
+            statement.setString(2, professor.getEmail());
+            statement.setDate(3, java.sql.Date.valueOf(professor.getDataNascimento()));
+            statement.setDouble(4, professor.getSalarioBase());
+            statement.setInt(5, professor.getCurso().getId());
+
+            int rowsAffected = statement.executeUpdate();
+
+            System.out.println("Done! " + rowsAffected + " row(s) affected.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
