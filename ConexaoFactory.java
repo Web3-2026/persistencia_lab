0471d8a15c115fc0a367dd6c084ca34c18d011e7
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2026-04-14 11:30:51 -0300

    Create ConexaoFactory.java

---
+++ ConexaoFactory.java
@@ -0,0 +1,20 @@
+public class ConexaoFactory {
+
+    public Connection getConnection(){
+        
+        String database = "";
+        String user = "";
+        String pass = ""
+
+        try {
+            
+            return DriverManager.getConnection("jdbc:mysql://localhost/".database, user, pass);
+
+        } catch (SQLException e) {
+            
+            throw new RuntimeException(e);
+        
+        }
+    }
+
+}
